d9c572b6f74858700a364319110473691ea51645
Dave Laing <dave.laing.80@gmail.com>
2014-04-23 20:07:20 +1000

    Updated the build script.
    Now locks the SHA-1 of reveal.js to the version that
    I originally used.

--- slides/build.sh
+++ slides/build.sh
@@ -2,6 +2,7 @@
 cabal install pandoc
 git clone https://github.com/hakimel/reveal.js
 pushd reveal.js
+git checkout 8b8cc607d4e02ca51c4256175b9cc861f75b70e3
 patch -p1 < ../reveal.patch
 popd
 pandoc -t revealjs -s lecture1.md -o lecture1.html
